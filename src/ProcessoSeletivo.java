1900df101de865c2686c86d9bf0c6363f2948d2f
pedrovsfelix <pedrovitorsf@gmail.com>
2024-04-28 17:22:44 -0300

    feat: modify class processoSeletivo.

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,9 +1,33 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
+
     public static void main(String[] args) {
         
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        // analisarCandidato(1900.0);
+        // analisarCandidato(2200.0);
+        // analisarCandidato(2000.0);
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos() {
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatosAtual < candidatos.length) {
+            double salarioPretendido = valorPretendido();
+            String candidato = candidatos[candidatosAtual];
+
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário " + salarioPretendido);
+            if(salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga.");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
     }
 
     static void analisarCandidato(double salarioPretendido) {
@@ -18,5 +42,9 @@ public class ProcessoSeletivo {
         }
 
         
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 }
