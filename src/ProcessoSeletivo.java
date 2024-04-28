fab2fcc61c3cc1dc64c6873a9379ace3796835b2
pedrovsfelix <pedrovitorsf@gmail.com>
2024-04-28 16:11:39 -0300

    feat: ProcessoSeletivo class..

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,5 +1,22 @@
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        System.out.println("Processo Seletivo");
+        
+        analisarCandidato(1900.0);
+        analisarCandidato(2200.0);
+        analisarCandidato(2000.0);
+    }
+
+    static void analisarCandidato(double salarioPretendido) {
+        double salarioBase = 2000.0;
+        
+        if (salarioBase > salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if (salarioBase == salarioPretendido) {
+            System.out.println("LIGAR PARA O CANDIDATO COM A CONTRA PROPOSTA");
+        }else {
+            System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS");
+        }
+
+        
     }
 }
